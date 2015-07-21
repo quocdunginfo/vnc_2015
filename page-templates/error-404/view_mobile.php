1a6cdd41c7b144630c8fc2448d2e7d59fa527dc5
Nguyễn Quốc Dũng <quocdunginfo@gmail.com>
2015-07-21 23:08:00 +0700

    Mobile: 404-ERROR Page

---
+++ page-templates/error-404/view_mobile.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: quocd_000
+ * Date: 24/06/2015
+ * Time: 8:52 PM
+ */
+QdT_Library::loadLayoutViewMobile('root');
+class QdT_PageT_Error404_ViewMobile extends QdT_Layout_Root_ViewMobile {
+    protected function getBannerPart()
+    {
+        //HIDE
+    }
+
+    protected function getBreadcrumbsPart()
+    {
+        //HIDE
+    }
+
+    protected function getContentPart()
+    {
+        ?>
+        <div class="container">
+            <div class="row">
+                <!--Left-->
+                <div class="col-xs-12" style="color: red; font-size: 14px">
+                    ERROR 404 - Resource not found
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+}
